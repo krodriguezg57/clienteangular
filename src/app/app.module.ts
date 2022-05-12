9c37eb8f1e391ee12d568c71523b0e0382d71c62
jjosegallegomesa <juan.gallegomesa@gmail.com>
2022-05-11 19:48:08 -0500

    se crea index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReservasComponent } from './reservas/reservas.component';
+import { FormularioreservasComponent } from './formularioreservas/formularioreservas.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReservasComponent,
+    FormularioreservasComponent,
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
